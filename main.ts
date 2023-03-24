253504600ccb557da53027c0a495fb77658c5bb6
Greggles182 <76397590+Greggles182@users.noreply.github.com>
2023-03-24 08:30:47 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+basic.showString("Connecting...")
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate115200)
+serial.writeLine("Connected!")
 basic.forever(function () {
-	
+    serial.writeLine("" + (input.temperature()))
+    serial.writeLine("" + (input.lightLevel()))
 })
